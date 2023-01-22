988d53b2cc683ecd08e30f4224dd2e467c102ff0
Zarina <catwarning@gmail.com>
2023-01-23 01:00:35 +0600

    Added Task 18 Задача 18

---
+++ Task18/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 18: Напишите программу, которая по
+// заданному номеру четверти, показывает диапазон
+// возможных координат точек в этой четверти (x и y).
+
+Console.WriteLine(" Введите номер чертверти: ");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+string Possiblecoor(int num)
+{
+    if (num == 1) return "x > 0, y > 0";
+    if (num == 2) return "x > 0, y < 0";
+    if (num == 3) return "x < 0, y < 0";
+    return " x < 0, y > 0 ";
+}
+if (quarter > 4 || quarter < 1)
+{
+    Console.WriteLine("Введите номер чертверти от 1 до 4: ");
+}
+
+else
+{
+    string possibleCoor = Possiblecoor(quarter);
+    Console.WriteLine(possibleCoor);
+}
