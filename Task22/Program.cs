d0c89e2069f6512f24b4ad3a2e4476a38e09d5cf
Zarina <catwarning@gmail.com>
2023-01-23 20:21:18 +0600

    Added Task 22 Задача 22

--- Task22/Program.cs
+++ Task22/Program.cs
@@ -1,29 +1,19 @@
-﻿// Math.Pow(2, 10);
-// Math.Sqrt(5);
-// double d = 5.09997623455;
-// double dRuynd = Math.Round(d, 2, MidpointRounding.ToZero);
-// System.Console.WriteLine(dRuynd);
+﻿// Задача 22: Напишите программу, которая
+// принимает на вход число (N) и выдаёт таблицу
+// квадратов чисел от 1 до N.
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
 
-// Задача 20: Напишите программу, 
-// 1.которая принимает на вход координаты двух точек и
-// 2.находит расстояние между ними в 2D пространстве.
-// A (3,6); B (2,1) -> 5,09
-// A (7,-5); B (1,-1) -> 7,21
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите координаты точки A: ");
-Console.Write("x1: "); int x1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("y1: "); int y1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите координаты точки B: ");
-Console.Write("x2: "); int x2 = Convert.ToInt32(Console.ReadLine());
-Console.Write("y2: "); int y2 = Convert.ToInt32(Console.ReadLine());
-
-// int double d = 5.09997623455;
-// double dRuynd = Math.Round(d, 2, MidpointRounding.ToZero);
-// System.Console.WriteLine(dRuynd);
-double distance = Distance(x1, y1, x2, y2);
-double distanceRound = Math.Round(distance, 2, MidpointRounding.ToZero);
-Console.WriteLine($"{distanceRound}");
-double Distance(int x1, int y1, int x2, int y2)
+void Square(int num)
 {
-    return Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+    int count = 1;
+    while (count <= num)
+    {
+        Console.WriteLine($"{count} {count * count}");
+        count++;
+    }
 }
+Square(number);
