c7b97c5cc8d8804adbfd1e49febf540aec9537db
Zarina <catwarning@gmail.com>
2023-01-24 18:05:03 +0600

    Added Task 26 Задача 26

---
+++ Task26/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 26: Напишите программу,
+// 1. которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.Write("Введите целое положительное число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    int sumDigit = SumDigit(number);
+    Console.WriteLine($"количество цифр в этом числе: {sumDigit}");
+}
+else Console.Write("Введено не коректное число");
+
+int SumDigit(int num)
+{
+    int count = 0;
+
+    while (num > 0)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
