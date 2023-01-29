13b1a0c01f05cb10739f1c52fdf7fef13599c534
Zarina <catwarning@gmail.com>
2023-01-29 13:26:54 +0600

    Added Task 33 Задача 33

---
+++ Task33/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 33: Задайте массив.
+// 1. Напишите программу, которая
+// 2. определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+int[] CreateArrayRnd(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length) Console.Write($"{array[i]}, ");
+    }
+    Console.WriteLine("]");
+}
+
+bool FindNumber(int[] array, int find)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == find)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Создание массива, ввод числа пользователем, вызов функции.
+Console.WriteLine("Укажите длину массива: ");
+int sizeArray = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumb = CreateArrayRnd(sizeArray, 1, 350);
+PrintArray(arrayNumb);
+Console.WriteLine("Укажите число: ");
+int numberToFind = Convert.ToInt32(Console.ReadLine());
+
+
+Console.WriteLine(FindNumber(arrayNumb, numberToFind) ? "да" : "нет");
