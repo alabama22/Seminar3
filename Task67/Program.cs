27ccb3c2893a796ada58f26c91eabce4719e3b6e
Zarina <catwarning@gmail.com>
2023-02-16 22:49:14 +0600

    Added Task 67 Задача 67

---
+++ Task67/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 67: Напишите программу, которая будет принимать 
+// на вход число и
+// возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+NaturalNumbers(number);
+int NaturalNumbers(int num)
+{
+    if (num == 0) return 0;
+    return num % 10 + NaturalNumbers(num / 10);
+}
+
+int numbersNatural = NaturalNumbers(number);
+Console.WriteLine(numbersNatural);
